133334cb52e232c95f4d0282b7e4a20d58531e20
Valentim Gomes <wlg1@outlook.pt>
2024-04-30 16:50:59 +0100

    ver final dia 30/04 - sem switch

--- Plugins/NIFPlugin.cs
+++ Plugins/NIFPlugin.cs
@@ -25,37 +25,46 @@ namespace Plugins
             IOrganizationServiceFactory serviceFactory =
                 (IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory));
             IOrganizationService service = serviceFactory.CreateOrganizationService(context.UserId);
-
-            // verificar quando o pop-up aparece, qual a message
-            if (context.MessageName == "create" /*|| context.MessageName == "update"*/)
-            {
-                // The InputParameters collection contains all the data passed in the message request.  
+            
+            // The InputParameters collection contains all the data passed in the message request.  
                 if (context.InputParameters.Contains("Target") &&
                 context.InputParameters["Target"] is Entity)
                 {
                     // Obtain the target entity from the input parameters.
                     Entity entity = (Entity)context.InputParameters["Target"];
-                    try
+                try
+                {
+                    if (!entity.Attributes.Contains("new_nif"))
                     {
-                        //verificar se o campo existe
-                        if (entity.Attributes.Contains("new_NIF"))
-                        {
+                        throw new InvalidPluginExecutionException("O campo NIF é obrigatório.");
+                    }
+                    else
+                    {
+                        if (entity.GetAttributeValue<string>("new_nif") == string.Empty)
+                        { 
                             throw new InvalidPluginExecutionException("O campo NIF é obrigatório.");
                         }
                     }
-
-                    catch (FaultException<OrganizationServiceFault> ex)
+                    var attributeValue = entity.GetAttributeValue<string>("new_nif");
+                    if (attributeValue == string.Empty)
                     {
-                        throw new InvalidPluginExecutionException("An error occurred in FillAField in the Plugins Solution.", ex);
-                    }
+                        tracingService.Trace("NIF: " + attributeValue);
+                        throw new InvalidPluginExecutionException("O campo NIF é obrigatório.");
 
-                    catch (Exception ex)
-                    {
-                        tracingService.Trace("FillAField: {0}", ex.ToString());
-                        throw;
                     }
                 }
-            }
+                catch (FaultException<OrganizationServiceFault> ex)
+                {
+                    throw new InvalidPluginExecutionException("An error occurred in FillAField in the Plugins Solution.", ex);
+                }
+
+                catch (Exception ex)
+                {
+                    tracingService.Trace("FillAField: {0}", ex.ToString());
+                    throw;
+                }
+                
+                }
                 
         }
     }
